418d9262cf4401d3ce96c2f40aef4bc0b0724f6f
Michael Vinci <michaeldvinci@gmail.com>
2015-03-10 00:43:38 -0400

    Initial Commit

---
+++ Aviato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aviato
+//
+//  Created by Michael Vinci on 3/10/15.
+//  Copyright (c) 2015 TeamAviato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
