ab9a0af503cdc543c89e2a83cc1605bbb7b2f367
Michael Vinci <michaeldvinci@gmail.com>
2015-03-13 15:31:02 -0400

    Implemented JSON Serialization for login JSON data to allow/deny login requests

--- Aviato/LoginViewController.m
+++ Aviato/LoginViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "LoginViewController.h"
-#import "CatViewController.h"
+#import "Login.h"
 
 @interface LoginViewController ()
 
@@ -15,9 +15,29 @@
 
 @implementation LoginViewController
 
+@synthesize UIPassword, UIUsername, locatServ, loginButton, loginArray, jLoginArray, locationManager, loginDict;
+
 - (void)viewDidLoad {
+    
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    
+    // Set a movement threshold for new events.
+    locationManager.distanceFilter = 500;
+    [locationManager startUpdatingLocation];
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+
+    didUpdateLocations:(NSArray *)locations {
+    }
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    //NSLog(@"%@",error);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,77 +45,55 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 - (IBAction)loginClicked:(id)sender {
-    NSInteger success = 0;
-    @try {
+    
+    NSString *success = @"0";
+    NSString *failure = @"0";
+    NSString *login = @"1";
+
+    NSString *loginURL = [@"http://tandemenvoy.michaeldvinci.com/forum/mobileSignin.php?" stringByAppendingString:[[NSString alloc] initWithFormat:@"userName=%@&userPass=%@",[self.UIUsername text],[self.UIPassword text]]];
+    
+    NSURL *dataURL = [NSURL URLWithString:loginURL];
+    NSData *loginData = [NSData dataWithContentsOfURL:dataURL];
+    
+    jLoginArray = [NSJSONSerialization JSONObjectWithData:loginData options:kNilOptions error:nil];
+    
+    for (loginDict in jLoginArray) {
+        NSString *success = loginDict[@"success"];
+    }
+    
+    loginArray = [[NSMutableArray alloc] init];
+    
+    for(int i = 0; i < jLoginArray.count; i++)
+    {
+        NSString *loginSuccess = [[jLoginArray objectAtIndex:i] objectForKey:@"success"];
         
-        if([[self.UIUsername text] isEqualToString:@""] || [[self.UIPassword text] isEqualToString:@""] ) {
-            
-            [self alertStatus:@"Please enter Email and Password" :@"Sign in Failed!" :0];
-            
-        } else {
-            
-            NSString *post =[[NSString alloc] initWithFormat:@"userName=%@&userPass=%@",[self.UIUsername text],[self.UIPassword text]];
-            
-            NSURL *url=[NSURL URLWithString:@"http://tandemenvoy.michaeldvinci.com/forum/mobileSignin.php"];
-            
-            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-            
-            NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-            
-            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-            [request setURL:url];
-            [request setHTTPMethod:@"GET"];
-            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-            [request setHTTPBody:postData];
-            
-            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
-            
-            NSError *error = [[NSError alloc] init];
-            NSHTTPURLResponse *response = nil;
-            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            
-            if ([response statusCode] >= 200 && [response statusCode] < 300)
-            {
-                NSError *error = nil;
-                NSDictionary *jsonData = [NSJSONSerialization
-                                          JSONObjectWithData:urlData
-                                          options:NSJSONReadingMutableContainers
-                                          error:&error];
-                
-                success = [jsonData[@"success"] integerValue];
-                
-                if(success == 1)
-                {
-                    NSLog(@"Login SUCCESS");
-                } else {
-                    
-                    NSString *error_msg = (NSString *) jsonData[@"error_message"];
-                    [self alertStatus:error_msg :@"Sign in Failed!" :0];
-                }
-                
-            } else {
-                if (error) NSLog(@"Error: %@", error);
-                [self alertStatus:@"Connection Failed" :@"Sign in Failed!" :0];
-            }
-        }
+        [loginArray addObject:[[Login alloc]initWidthSuccess: loginSuccess]];
     }
-    @catch (NSException * e) {
-        NSLog(@"Exception: %@", e);
-        [self alertStatus:@"Sign in Failed." :@"Error!" :0];
-    }
-    if (success == 1) {
+    
+    success = [[jLoginArray objectAtIndex:0]objectForKey:@"success"];
+    
+    NSLog(@"Success: %@", success);
+    
+    if([success isEqualToString:(login)]) {
         [self performSegueWithIdentifier:@"login_success" sender:self];
+    }
+    else {
+        [self showAlert];
     }
 }
 
-- (void) alertStatus:(NSString *)msg :(NSString *)title :(int) tag
-{
-    CatViewController *homeView = [[CatViewController alloc] init];
-    [self.navigationController pushViewController:homeView animated:YES];
+- (void) showAlert {
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+                          
+                          initWithTitle:@"Login Failure"
+                          message:@"Wrong username/password, please try again"
+                          delegate:nil
+                          cancelButtonTitle:@"Dismiss"
+                          otherButtonTitles:nil];
+    
+    [alert show];
 }
 
 @end
