c11ab9273eefd221be1b25441fbcb63d0cd2927a
Michael Vinci <michaeldvinci@gmail.com>
2015-03-13 15:30:22 -0400

    Implemented new properties for better Login handling

--- Aviato/LoginViewController.h
+++ Aviato/LoginViewController.h
@@ -7,10 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : UIViewController <CLLocationManagerDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *UIUsername;
 @property (weak, nonatomic) IBOutlet UITextField *UIPassword;
+@property (nonatomic, strong) NSString *locatServ;
+@property (nonatomic, strong) IBOutlet UIButton *loginButton;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) NSMutableArray *jLoginArray;
+@property (nonatomic, strong) NSMutableArray *loginArray;
+@property (nonatomic, strong) NSDictionary *loginDict;
+
 
 - (IBAction)loginClicked:(id)sender;
 
