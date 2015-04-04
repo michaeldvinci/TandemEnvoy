3e23d95f05a5a49206f3e261d7a5c2a84cedfd15
Michael Vinci <michaeldvinci@gmail.com>
2015-04-04 15:36:13 -0400

    'Disclaimer' added

---
+++ Aviato/Disclaimer.h
@@ -0,0 +1,11 @@
+//
+//  Disclaimer.h
+//  Aviato
+//
+//  Created by Michael Vinci on 4/4/15.
+//  Copyright (c) 2015 TeamAviato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@end
---
+++ Aviato/Disclaimer.m
@@ -0,0 +1,9 @@
+//
+//  Disclaimer.m
+//  Aviato
+//
+//  Created by Michael Vinci on 4/4/15.
+//  Copyright (c) 2015 TeamAviato. All rights reserved.
+//
+
+
