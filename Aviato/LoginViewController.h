7b96d4d4b3d0fbb6df15362c30082b2e7ddd3dce
Michael Vinci <michaeldvinci@gmail.com>
2015-04-15 16:31:43 -0400

    Documentation

--- Aviato/LoginViewController.h
+++ Aviato/LoginViewController.h
@@ -8,19 +8,69 @@
 
 #import <UIKit/UIKit.h>
 
+/*!
+ *	LoginViewController object to allow setting of LVC objects during degue
+ */
 @interface LoginViewController : UIViewController
 
+/*!
+ *	User singleton that returns user data
+ */
 @property (nonatomic, strong) User *user;
+
+/*!
+ *	Text Field from which login userName is pulled
+ */
 @property (weak, nonatomic) IBOutlet UITextField *UIUsername;
+
+/*!
+ *	Text Field from which login userPass is pulled
+ */
 @property (weak, nonatomic) IBOutlet UITextField *UIPassword;
+
+/*!
+ *	variable for location service to be called upon
+ */
 @property (nonatomic, strong) NSString *locatServ;
+
+/*!
+ *	Button which initiates the loginClicked IBAction
+ */
 @property (nonatomic, strong) IBOutlet UIButton *loginButton;
+
+/*!
+ *	JSON array of login crendentials
+ */
 @property (nonatomic, strong) NSMutableArray *jLoginArray;
+
+/*!
+ *	URL of which to connect to .php file
+ */
 @property (nonatomic, strong) NSString *loginURL;
+
+/*!
+ *	Credentials for JSON data and $_POST request
+ */
 @property (nonatomic, strong) NSURLCredential *credential;
+
+/*!
+ *	Credential persistence for session information
+ */
 @property (nonatomic) NSURLCredentialPersistence *permanent;
 
+/*!
+ *	when activated, it will send data to .php file to check for proper credentials to let the user log in, if 
+ *  proper cred. are found, user will segue to the CatViewController
+ *
+ *	@param sender sender is the user data
+ */
 - (IBAction)loginClicked:(id)sender;
+
+/*!
+ *	when activated, it will send user to the RegisterViewController allowing them to sign up for a new account
+ *
+ *	@param sender sender is the user data
+ */
 - (IBAction)registerClicked:(id)sender;
 
 @end
--- Aviato/LoginViewController.m
+++ Aviato/LoginViewController.m
@@ -16,8 +16,16 @@
 
 @implementation LoginViewController
 
+/*!
+ *	Creates a getter and a setter for each variable
+ */
 @synthesize UIPassword, UIUsername, locatServ, loginButton, jLoginArray, user, loginURL, credential, permanent;
 
+
+/*!
+ *	Initializes the ViewController and sets it so that there is no navigation bar for no accidental return to the Login
+ *  Screen. Also hides back button as a second precaution.
+ */
 - (void)viewDidLoad {
     
     self.navigationController.navigationBar.hidden = YES;
@@ -29,24 +37,41 @@
     self.navigationItem.hidesBackButton = YES;
 }
 
+
+/*!
+ *	Checks to see upon view loading, if there was an error about memory allocation
+ */
 - (void)didReceiveMemoryWarning {
     
     [super didReceiveMemoryWarning];
     
 }
 
+/*!
+ *	When interacted with, it sends the 'userName' and 'userPass' to the login.php files to check for proper credentials
+ *
+ *	@param sender sender is the User
+ */
 - (IBAction)loginClicked:(id)sender {
     
     [self login];
     
     }
 
+/*!
+ *	When interacted with, it changes the view to the RegisterNewAccount View
+ *
+ *	@param sender User is the sender
+ */
 - (IBAction)registerClicked:(id)sender {
     
     [self registerNew];
 
 }
 
+/*!
+ *	If the incorrect credentials are returned, the alert will popup and inform the user
+ */
 - (void)showAlert {
     
     UIAlertView *alert = [[UIAlertView alloc]
@@ -60,12 +85,18 @@
     [alert show];
 }
 
+/*!
+ *	calls the 'registerMe' segue to change views
+ */
 - (void) registerNew {
     
     [self performSegueWithIdentifier:@"registerNew" sender:self];
     
 }
 
+/*!
+ *	checks for proper credentials and determines if the user moves to the next view or not
+ */
 - (void)login {
     
     NSURL *feedsURL = [NSURL URLWithString:@"http://tandemenvoy.michaeldvinci.com/forum/signin2.php"];
@@ -110,6 +141,10 @@
     
 }
 
+
+/*!
+ *	test method to see what variables are being returned
+ */
 - (void) test {
     NSLog(@" ");
     NSLog(@"----------------------");
