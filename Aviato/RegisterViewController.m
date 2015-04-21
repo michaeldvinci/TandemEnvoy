9a2deccd517c8a324fa33acc4c955f5f0d3c89e0
Michael Vinci <michaeldvinci@gmail.com>
2015-04-21 17:41:39 -0400

    Implemented auto-login upon successful User Registration

--- Aviato/RegisterViewController.m
+++ Aviato/RegisterViewController.m
@@ -8,6 +8,7 @@
 
 #import "User.h"
 #import "RepoViewController.h"
+#import "LoginViewController.h"
 #import "RegisterViewController.h"
 
 @interface RegisterViewController () <RegisterViewControllerDelegate>
@@ -17,10 +18,12 @@
 @implementation RegisterViewController {
 }
 
-@synthesize userName, userPass, userEmail, userPassCheck;
+@synthesize userName, userPass, userEmail, userPassCheck, user;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+    
+    user = [User getInstance];
 }
 
 - (IBAction)textFieldReturn:(id)sender {
@@ -50,25 +53,47 @@
 }
 
 /*!
+ *	test method to see what variables are being returned
+ */
+- (void)test {
+    NSLog(@" ");
+    NSLog(@"----------------------");
+    NSLog(@"User Name: %@", user.userName);
+    NSLog(@"User ID: %@", user.userID);
+    NSLog(@"----------------------");
+    NSLog(@" ");
+}
+
+/*!
  *	Shows alert when Successful user added to DB
  */
 - (void)showAlert1 {
-	UIAlertView *alert = [[UIAlertView alloc]
+	UIAlertView *alert1 = [[UIAlertView alloc]
 
 	                      initWithTitle:@"Successful"
 	                                message:@"New user registered!"
-	                               delegate:nil
-	                      cancelButtonTitle:@"Dismiss"
-	                      otherButtonTitles:nil];
+	                               delegate:self
+	                      cancelButtonTitle:nil
+	                      otherButtonTitles:@"OK", nil];
+    alert1.tag = 100;
+	[alert1 show];
+}
 
-	[alert show];
+- (void) alertView: (UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    if (alertView.tag == 100) {
+        if (buttonIndex == 0) {
+            [self login];
+            [self test];
+        }
+    }
 }
 
 /*!
  *	Shows alert when failure at adding new user
  */
 - (void)showAlert2 {
-	UIAlertView *alert = [[UIAlertView alloc]
+	UIAlertView *alert2 = [[UIAlertView alloc]
 
 	                      initWithTitle:@"Failure"
 	                                message:@"Registration attempt failed, try again"
@@ -76,14 +101,14 @@
 	                      cancelButtonTitle:@"Dismiss"
 	                      otherButtonTitles:nil];
 
-	[alert show];
+	[alert2 show];
 }
 
 /*!
  *	Shows alert if there is already a user with the same name in the DB
  */
 - (void)showAlert3 {
-	UIAlertView *alert = [[UIAlertView alloc]
+	UIAlertView *alert3 = [[UIAlertView alloc]
 
 	                      initWithTitle:@"Failure"
 	                                message:@"userName already taken, try again"
@@ -91,7 +116,26 @@
 	                      cancelButtonTitle:@"Dismiss"
 	                      otherButtonTitles:nil];
 
-	[alert show];
+	[alert3 show];
+}
+
+- (void)login {
+    NSString *loginURL = [@"http://tandemenvoy.michaeldvinci.com/forum/mobileSignin.php?" stringByAppendingString:[[NSString alloc] initWithFormat:@"userName=%@&userPass=%@", userName.text, userPass.text]];
+    
+    NSURL *dataURL = [NSURL URLWithString:loginURL];
+    NSData *loginData = [NSData dataWithContentsOfURL:dataURL];
+    
+    NSMutableArray *jLoginArray = [NSJSONSerialization JSONObjectWithData:loginData options:kNilOptions error:nil];
+    
+    user.userName = userName.text;
+    user.userID = [[jLoginArray objectAtIndex:0]objectForKey:@"userID"];
+    
+    NSString *success = [[jLoginArray objectAtIndex:0] objectForKey:@"success"];
+    
+    if ([success isEqualToString:@"1"]) {
+        [self performSegueWithIdentifier:@"loginSuccess" sender:self];
+    }
+
 }
 
 /*!
@@ -113,35 +157,37 @@
 
 	User *user1 = [User getInstance];
 
-	NSString *emailString = [userEmail.text stringByReplacingOccurrencesOfString:@"@" withString:@"%40"];
-	emailString = [emailString stringByReplacingOccurrencesOfString:@"." withString:@"%2E"];
+    if ([userPass.text isEqualToString:userPassCheck.text]) {
+        NSString *emailString = [userEmail.text stringByReplacingOccurrencesOfString:@"@" withString:@"%40"];
+        emailString = [emailString stringByReplacingOccurrencesOfString:@"." withString:@"%2E"];
 
-	NSString *myRequestString = [NSString stringWithFormat:@"userName=%@&userPass=%@&userEmail=%@", userName.text, userPass.text, emailString];
+        NSString *myRequestString = [NSString stringWithFormat:@"userName=%@&userPass=%@&userEmail=%@", userName.text, userPass.text, emailString];
 
-	NSLog(@"Link: %@", myRequestString);
+        NSLog(@"Link: %@", myRequestString);
 
-	NSData *myRequestData = [NSData dataWithBytes:[myRequestString UTF8String] length:[myRequestString length]];
-	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://tandemenvoy.michaeldvinci.com/forum/iosSignup.php"]];
-	[request setHTTPMethod:@"POST"];
-	[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
-	[request setHTTPBody:myRequestData];
-	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-	NSString *response = [[NSString alloc] initWithBytes:[returnData bytes] length:[returnData length] encoding:1];
-	NSLog(@"%@", response);
+        NSData *myRequestData = [NSData dataWithBytes:[myRequestString UTF8String] length:[myRequestString length]];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://tandemenvoy.michaeldvinci.com/forum/iosSignup.php"]];
+        [request setHTTPMethod:@"POST"];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+        [request setHTTPBody:myRequestData];
+        NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSString *response = [[NSString alloc] initWithBytes:[returnData bytes] length:[returnData length] encoding:1];
+        NSLog(@"%@", response);
 
-	NSArray *registerArray = [NSJSONSerialization JSONObjectWithData:returnData options:kNilOptions error:nil];
+        NSArray *registerArray = [NSJSONSerialization JSONObjectWithData:returnData options:kNilOptions error:nil];
 
-	NSString *success = [[registerArray objectAtIndex:0] objectForKey:@"success"];
+        NSString *success = [[registerArray objectAtIndex:0] objectForKey:@"success"];
 
-	if ([success isEqualToString:@"1"]) {
-		[self showAlert1];
-	}
-	else if ([success isEqualToString:@"0"]) {
-		[self showAlert2];
-	}
-	else {
-		[self showAlert3];
-	}
+        if ([success isEqualToString:@"1"]) {
+            [self showAlert1];
+        }
+        else if ([success isEqualToString:@"0"]) {
+            [self showAlert2];
+        }
+        else {
+            [self showAlert3];
+        }
+    }
     
     
 	[self.navigationController popViewControllerAnimated:YES];
